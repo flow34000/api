e2e77ded21d1f6c2caef8e9a756d86b3ddf30e6e
Florent Guillaume <guillaume.florent@live.fr>
2016-02-02 14:04:40 +0100

    test

--- src/main/java/model/PatientModel.java
+++ src/main/java/model/PatientModel.java
@@ -1,10 +1,42 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import persistence.HibernateUtil;
 import domain.Patient;
 
-public class PatientModel extends AbstractModel<Patient, Short> {
+public class PatientModel {
 
 	public PatientModel(){
-		super(Patient.class);
+		
+	}
+	
+	public List <Patient> findAll() throws Exception{
+		Session session = HibernateUtil.currentSession();
+		try{
+			Transaction tx = session.beginTransaction();
+			try{
+				List<Patient> patients = new ArrayList<Patient>();
+				patients = (List<Patient>) session.createQuery("from patient");
+				
+				int size = patients.size();
+				for(int i=0;i<size;i++){
+					Hibernate.initialize(patients.get(i).getComptages());
+				}
+				
+				tx.commit();
+				return patients;
+			}catch(Exception ex){
+				tx.rollback();
+				throw(ex);
+			}
+		}finally{
+			HibernateUtil.closeSession();
+		}
 	}
 }
